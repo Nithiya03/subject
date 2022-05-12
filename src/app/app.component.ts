82a8973253c87d3fb630685da6ad85ef04172f8b
Nithiya03 <nithiyasrirud03@gmail.com>
2022-05-12 18:00:24 +0530

    angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable, Subject,BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'subject';
+
+  ngOnInit(){
+    const obs = new Observable(obj => obj.next(Math.random()));
+    console.log("Observable");
+    obs.subscribe(d => console.log(d));
+    obs.subscribe(d => console.log(d));
+
+    const sub = new Subject();
+    console.log("subject");
+    sub.subscribe(d => console.log(d));
+    sub.subscribe(d => console.log(d));
+    sub.next(Math.random());
+
+    const bsub = new BehaviorSubject(12);
+
+    console.log("behaviour subject");
+    bsub.subscribe(d=>console.log(d));
+
+    bsub.next(200);
+    console.log("behaviour subject2");
+    bsub.subscribe(d=>console.log(d));
+
+  }
 }
